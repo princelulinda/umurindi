071a52119dbf5a6e8bcab0787d73d16defdd3225
princelulinda <princelulinda@gmail.com>
2025-03-27 17:31:14 +0200

    re design

--- app/auth/activate/[uidb64]/[token]/page.tsx
+++ app/auth/activate/[uidb64]/[token]/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useActivateQuery } from '@/redux/features/authApiSlice';
 import { toast } from 'react-toastify';
 
 interface Props {
@@ -16,7 +15,7 @@ export default function Page({ params }: Props) {
 	const { uidb64, token } = params;
   
 	const router = useRouter();
-	const { data, error, isLoading } = useActivateQuery({ uidb64, token });
+	// const { data, error, isLoading } = useActivateQuery({ uidb64, token });
 
 
 
@@ -34,7 +33,7 @@ export default function Page({ params }: Props) {
 		<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
 			<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
 				<h1 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
-					{isLoading ? 'Activation de votre compte en cours...' : 'Redirection...'}
+					{/* {isLoading ? 'Activation de votre compte en cours...' : 'Redirection...'} */}
 				</h1>
 			</div>
 		</div>
